cbb9641fb58a6c79e5d1ec3f27ac723404e8ac36
Sushant Saini (Nokia) <sushant.saini@nokia.com>
2026-02-22 12:32:04 +0530

    added comments

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,17 +10,19 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
-        script.echo "building the docker image..."
+        script.echo "building the docker image... through sharedlib"
         script.sh "podman-remote build --build-arg HTTP_PROXY=${script.env.HTTP_PROXY} --build-arg HTTPS_PROXY=${script.env.HTTPS_PROXY} --build-arg NO_PROXY=${script.env.NO_PROXY} -t $imageName ."
         }
 
     def dockerLogin() {
+        scritp.sh 'Dcoker login'
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo '${script.PASS}' | podman-remote login -u '${script.USER}' --password-stdin"
         }
     }
 
     def dockerPush(String imageName) {
+        script.sh 'Pushing image to docker repo from sharedlib'
         def newImage= "docker.io/sushantsaini29/${imageName}"
         script.sh "podman-remote tag localhost/${imageName} ${newImage}"
         script.sh "podman-remote push ${newImage} --tls-verify=false"
