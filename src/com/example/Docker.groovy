67cb5dede9417edfecf1593f0daa60eacfac30a7
Sushant Saini (Nokia) <sushant.saini@nokia.com>
2026-02-22 12:42:16 +0530

    corrected spellings

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -15,7 +15,7 @@ class Docker implements Serializable {
         }
 
     def dockerLogin() {
-        script.sh "Dcoker login in shred lib"
+        script.sh "Docker login in shared lib"
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo '${script.PASS}' | podman-remote login -u '${script.USER}' --password-stdin"
         }
